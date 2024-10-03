0ec6f258b6f0e0799c5d76a37cb2956c10b998a8
DjYasi22 <yasidj050695@gmail.com>
2024-10-03 12:02:28 +0800

    hello

---
+++ Task1.py
@@ -0,0 +1,7 @@
+fruits = int(input("How many favorite fruits you would like:"))
+fruits = []
+
+for data in range(fruits)
+    fruits = input ("enter name of fruits")
+    fruits.append(name)
+print(fruits)
